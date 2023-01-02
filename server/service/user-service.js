915e257ac543a16704d3ba9bda44f23bc3587192
michael-916310 <916310@rambler.ru>
2023-01-02 14:05:59 +0400

    UserController.registration works

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,7 +1,9 @@
-const UserModel = require('../models/user-model');
+const UserModel = require('../model/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dto/user-dto');
 
 class UserService {
     async registration(email, password) {
@@ -12,8 +14,18 @@ class UserService {
 
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
+
         const user = await UserModel.create({email, password: hashPassword, activationLink});
         await mailService.sendActivationMail(email, activationLink);
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 
 }
